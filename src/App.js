47a075cecd7daf7dbd665bad8657f07985b8358f
Lida Aupova <lida.aupova@gmail.com>
2023-04-29 22:28:44 +0300

    New Material UI test project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Button from '@mui/material/Button';
+import { AppBar, Box, Container, IconButton, Toolbar, Typography } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import { makeStyles } from 'tss-react/mui';
 
-function App() {
+
+const useStyles = makeStyles()((theme) => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(1) //1 - по умолчанию 8px
+  },
+  title: {
+    flexGrow: 1
+  }
+}))
+
+const App = () => {
+  const {classes} = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppBar position='fixed'> {/*Панель навигации, используется для логотипа, заголовка, навигации и т.д. Может быть контекстным меню или навигационным*/}
+      <Container fixed> {/*Центрирует содержимое по горизонтали*/}
+        <Toolbar> {/*Элемент, в который обычно кладутся иконки, логотипы и кнопки для адаптивного меню*/}
+          <IconButton
+            edge='start'
+            color='inherit'
+            aria-label='menu'
+            className={classes.menuButton}
+          >
+            <MenuIcon/>
+          </IconButton>
+          <Typography
+            variant='h6'
+            className={classes.title}
+          >
+            Web Developer Blog
+          </Typography> {/*Компонент, в котором мы записываем текст*/}
+          <Box mr={3}> {/*Компонент, который используется как обёртка для большинства используемых css-свойст, т.е. создаёт новые DOM элемент, по умолчанию div, но можно изменить свойством component*/}
+            <Button color='inherit' variant='outline'>Log in</Button>
+          </Box>
+          <Button color='secondary' variant='contained'>Sign up</Button>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 }
 
